9ab160c8760f3c7597f10c5dd41ad9cf83c351ea
Jerome <654321su@gmail.com>
2018-11-03 21:43:03 +0900

    #010 Strings and Variables

---
+++ basics/strings.js
@@ -0,0 +1,23 @@
+/**
+ *  Creating Variables
+ *  String Concatenation
+ */
+
+
+let firstName = 'Jerome';
+let lastName = 'Moon';
+let fullName = firstName + ' ' +  lastName;
+
+console.log(fullName);
+
+// Create a variable to store the city
+let city = 'Seoul';
+
+// Create a variable to store the state
+let country = 'Korea';
+
+// Create a variable to combine the city and state
+let location = city + ', ' + country;
+
+// Print the combined location to the terminal
+console.log(location);
