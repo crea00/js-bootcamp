d92c0788c439623c701d5026ff694a734f766d05
Jerome <654321su@gmail.com>
2018-11-06 20:46:47 +0900

    #014 Booleans and Comparison Operators

---
+++ basics/boolean.js
@@ -0,0 +1,31 @@
+/**
+ *  Booleans and Comparison Operators
+ * 
+ *  === - equality operator
+ *  !== - not equal operator
+ *  < - less than operator
+ *  > - greater than operator
+ *  <= - less than or equal to operator
+ *  >= - greater than or equal to operator
+ *    
+ */
+
+
+let temp = 31;
+let isFreezing = temp
+
+console.log(isFreezing);
+
+
+// Challenge area
+
+// Create age set to your age
+let age = 29;
+// Calculate is child - if they are 7 or under
+let isChild = age <= 7;
+// Calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+// Print is child value
+console.log(isChild);
+// Print is senior value
+console.log(isSenior);
