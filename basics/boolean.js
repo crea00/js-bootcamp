9bc144f46020c99950a2fafaf8451498d6dfe1c3
Jerome <654321su@gmail.com>
2018-11-07 01:30:47 +0900

    #015 If Statements

--- basics/boolean.js
+++ basics/boolean.js
@@ -11,21 +11,26 @@
  */
 
 
-let temp = 31;
-let isFreezing = temp
+let temp = 180;
 
-console.log(isFreezing);
+if(temp <= 32) {
+  console.log('It is freezing outside!');
+}
+
+if(temp >= 110) {
+  console.log('It is way to hot outside!');
+}
 
 
 // Challenge area
 
-// Create age set to your age
-let age = 29;
-// Calculate is child - if they are 7 or under
-let isChild = age <= 7;
-// Calculate is senior - if they are 65 or older
-let isSenior = age >= 65;
-// Print is child value
-console.log(isChild);
-// Print is senior value
-console.log(isSenior);
+let age = 65;
+
+// If 7 or under print message about child pricing
+if(age <= 7) {
+  console.log('You will get a child discount!');
+}
+// If 65 or older print message senior discount
+if(age >= 65) {
+  console.log('You will get a senior discount!');
+}
