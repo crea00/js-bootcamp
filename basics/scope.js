2ead1ca7093775383e9e9e81c62f7be6cec81706
Jerome <654321su@gmail.com>
2018-11-10 20:15:02 +0900

    #018 Variable Scope: Part I

---
+++ basics/scope.js
@@ -0,0 +1,18 @@
+/**
+ *  Variable Scope
+ * 
+ *  Lexical Scope (Static Scope)
+ *  Global Scope - Defined outside of all code blocks
+ *  Local Scope - Defined inside a code block
+ */
+
+// Global Scope
+let varOne = 'varOne';
+
+if(true) {
+  console.log(varOne);
+  let varTwo = 'varTwo';
+  console.log(varTwo);
+}
+
+console.log(varTwo);
