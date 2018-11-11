b90050b44cd14d0f1c5290ccee2dcc0db8aa9ddb
Jerome <654321su@gmail.com>
2018-11-11 23:54:25 +0900

    #025 Template Strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -12,7 +12,7 @@ console.log(result);
 
 // Default arguments
 let getScoreText = function(name = 'Anonymous', score = 0) {
-  return 'Name: ' + name + ' - Score: ' + score;
+  return `Name: ${name} - Score: ${score}`;
 };
 
 let scoreText = getScoreText(99);
@@ -21,9 +21,12 @@ console.log(scoreText);
 
 // Challenge area
 
+// A 25% temp on $40 would be $10 
 let getTip = function(total, tipPercent = .2) {
-  return total * tipPercent;
+  let percent = tipPercent * 100;
+  let tip = total * tipPercent;
+  return `A ${tipPercent * 100}% tip on ${total} would be ${tip}`;
 };
 
 let tip = getTip(40, .25);
-console.log(tip);
+console.log(tip); 
