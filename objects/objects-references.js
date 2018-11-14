3cea8c0eea8772555075619b63a000fb3f798f69
Jerome <654321su@gmail.com>
2018-11-15 00:33:49 +0900

    #031 Build a Expense Tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -7,13 +7,32 @@ let myAccount = {
   expenses: 0,
   income: 0
 };
-let otherAccount = myAccount;
-otherAccount.income = 1000;
-otherAccount = {};
 
 let addExpense = function(account, amount) {
   account.expenses = account.expenses + amount;
 };
 
-addExpense(myAccount, 2.5);
-console.log(myAccount);
+// addIncome
+let addIncome = function(account, income) {
+  account.income = account.income + income;
+};
+
+// resetAccount
+let resetAccount = function(account) {
+  account.expenses = 0;
+  account.income = 0;
+};
+
+// getAccountSummary
+let getAccountSummary = function(account) {
+  let balance = account.income - account.expenses;
+  return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses`;
+};
+// Account for Andrew has $900. $1000 in income. $100 in expenses
+
+addIncome(myAccount, 2000);
+addExpense(myAccount, 2.50);
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
