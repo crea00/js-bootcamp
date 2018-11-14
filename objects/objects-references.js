fb67935c527acd119225f2b3d210cd17625ebb7a
Jerome <654321su@gmail.com>
2018-11-14 21:13:48 +0900

    #030 Object References

---
+++ objects/objects-references.js
@@ -0,0 +1,19 @@
+/**
+ *  Object References
+ */
+
+let myAccount = {
+  name: 'Jerome',
+  expenses: 0,
+  income: 0
+};
+let otherAccount = myAccount;
+otherAccount.income = 1000;
+otherAccount = {};
+
+let addExpense = function(account, amount) {
+  account.expenses = account.expenses + amount;
+};
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
