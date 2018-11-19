eeca9ae6f8da7480509da08c52c61720a34abac3
Jerome <654321su@gmail.com>
2018-11-20 01:08:35 +0900

    #041 The For Loop

--- arrays/todo.js
+++ arrays/todo.js
@@ -6,12 +6,19 @@ const todos = [
   'Exercise'
 ];
 
-todos.splice(2, 1);
-todos.push('Buy coffee');
-todos.shift();
+// todos.splice(2, 1);
+// todos.push('Buy coffee');
+// todos.shift();
 
 console.log(`You have ${todos.length} todos!`);
 
 todos.forEach(function(todo, index) {
-  console.log(`${index + 1}. ${todo}`);
+  const num = index + 1;
+  console.log(`${num}. ${todo}`);
 });
+
+for(let count = 0; count < todos.length; count++) {
+  const num = count + 1;
+  const todo = todos[count];
+  console.log(`${num}. ${todo}`);
+};
