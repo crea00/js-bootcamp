03fc347fe8c8621fc2c11279a46df7c7c9b3e87b
Jerome <654321su@gmail.com>
2018-11-25 20:27:32 +0900

    #051 DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,14 @@
-console.log('This is from a different file');
+// DOM - Document Object Model
+
+// Query and Remove
+// const p = document.querySelector('p');
+// p.remove();
+
+// Query all and Remove
+const ps = document.querySelectorAll('p');
+
+ps.forEach(function(p) {
+  p.textContent = '******';
+  // console.log(p.textContent); 
+  // p.remove();
+});
