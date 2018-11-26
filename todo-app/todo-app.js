f52b8e31f1b5a5fb0603d5f73a181333f96d612d
Jerome <654321su@gmail.com>
2018-11-26 09:09:55 +0900

    #054 Advanced Queries

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -33,6 +33,6 @@ todos.forEach(function(todo) {
 });
 
 // Listen for new todo creation
-document.querySelector('button').addEventListener('click', function(e) {
+document.querySelector('#add-todo').addEventListener('click', function(e) {
   console.log('Add a new todo...');
 });
